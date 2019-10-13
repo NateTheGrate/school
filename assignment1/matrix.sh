12aec5fd529f59528859ec8887b2752401f1c149
NateTheGrate <njbutler99@gmail.com>
2019-10-12 18:17:21 -0700

    added add function

--- assignment1/matrix.sh
+++ assignment1/matrix.sh
@@ -21,25 +21,63 @@ dims()
 
 transpose()
 {
-    result="transpose_result$$"
+    tresult="_transpose_result$$"
     cols="$(dims $1 | cut -d ' ' -f 2)"
 
     for (( i=1; i<=$cols; i++ )); do
         
         column="$(cat $mat | cut -f $i)"
-        echo $column | tr -s ' ' '\t' >> $result
+        echo $column | tr -s ' ' '\t' >> $tresult
         
     done
 
-    cat $result
-    rm $result
+    cat $tresult
     return 0
 }
 
-## start of main script
+add()
+{
+    aresult="_addition$$"
+    cols="$(dims $2 | cut -d ' ' -f 2)"
+    rows="$(dims $2 | cut -d ' ' -f 1)"
+    
+    arr1=()
+    while read -a row; do
+        for i in "${row[@]}"; do
+            
+            arr1+=( $i )
+
+        done
+    done < $1
+    
+    arr2=()
+    while read -a row; do
+        for i in "${row[@]}"; do
+            
+            arr2+=( $i )
+
+        done
+    done < $2
+    
+    j=0
+    for (( i=0; i<$rows*$cols; i++ )); do
+
+        if (( ($i+1)%$cols == 0 )); then 
+            printf "%d\n" `expr ${arr1[i]} + ${arr2[i]}` >> $aresult
+        else
+            printf "%d\t" `expr ${arr1[i]} + ${arr2[i]}` >> $aresult
+        fi
+    done
+    
+    cat $aresult
+    return 0
+
+}
+
+## start of main script ##
 
 if [ "$1" == "dims" ]; then
-    mat="input_matrix$$"
+    mat="_input_matrix$$"
     if  [ $# -eq 1 ]; then
         cat > $mat
     elif [ $# -eq 2 ]; then
@@ -57,7 +95,7 @@ if [ "$1" == "dims" ]; then
     dims $mat
 
 elif [ "$1" == "transpose" ]; then
-    mat="input_matrix$$"
+    mat="_input_matrix$$"
      if  [ $# -eq 1 ]; then
         cat > $mat
     elif [ $# -eq 2 ]; then
@@ -74,9 +112,33 @@ elif [ "$1" == "transpose" ]; then
 
     transpose $mat
     
+elif [ "$1" == "add" ]; then
+    
+    if (( $# < 3 )); then
+
+        echo "invalid arguments" 1>&2
+        exit 1
+
+    fi
+
+    cols1="$(dims $2 | cut -d ' ' -f 2)"
+    rows1="$(dims $2 | cut -d ' ' -f 1)"
+
+    cols2="$(dims $3 | cut -d ' ' -f 2)"
+    rows2="$(dims $3 | cut -d ' ' -f 1)"
+
+
+    if [[ "$cols2" != "$cols1" || "$rows1" != "$rows2" ]]; then
+        echo "mismatched matricies" 1>&2
+        exit 1
+    fi
+    add $2 $3
+
 else
     echo "invalid operator" 1>&2
     exit 1
 fi
 
+
+rm -f _*
 exit 0
